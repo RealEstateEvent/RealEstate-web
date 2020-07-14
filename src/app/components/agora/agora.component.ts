4ef4343fd80d208dd8a6ebe39709ff772d47da2b
ongraph <kapil.upwork@gmail.com>
2020-07-15 00:44:05 +0530

    changes for agora

--- src/app/components/agora/agora.component.ts
+++ src/app/components/agora/agora.component.ts
@@ -1,53 +1,65 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import * as AgoraRTM from 'agora-rtm-sdk';
 import * as AgoraRTC from 'agora-rtc-sdk'
 import { environment } from '../../../environments/environment';
+import { AuthService } from '../../shared/services/internal/auth/auth.service';
+import { DataSharedService } from '../../shared/services/internal/data-shared/data-shared.service';
 
 @Component({
   selector: 'app-agora',
   templateUrl: './agora.component.html',
   styleUrls: ['./agora.component.scss']
 })
-export class AgoraComponent implements OnInit {
-  evt_id;
+export class AgoraComponent implements OnInit, OnDestroy {
+
+  evt_data;
   agenda_id;
   chatClient;
   chatChannel;
+  role;
   streamClient;
   localStream;
+  user_id;
   remoteStreams = [];
   @ViewChild('message', { static: true }) message: ElementRef;
 
   constructor(private route: ActivatedRoute,
-    private router: Router) {
-    this.evt_id = this.router.getCurrentNavigation().extras.state.evt_id;
+    private router: Router,
+    private authService: AuthService,
+    private dataSharedService: DataSharedService) {
+    this.dataSharedService.getEventData().subscribe((evt) => {
+      this.evt_data = evt;
+    })
+    this.user_id = this.authService.getUserIDFromToken();
+    alert(this.user_id);
     this.chatClient = AgoraRTM.createInstance(environment.agoraAppID);
     this.streamClient = AgoraRTC.createClient({ mode: "live", codec: "h264" });
   }
 
   ngOnInit(): void {
+    this.dataSharedService.setAgoraStatus(true);
     this.agenda_id = this.route.snapshot.queryParams.meetingid;
+    //role can be of "host" or "audience".
+    this.role = "host";
     this.startCall();
+  }
+
+  ngOnDestroy(): void {
+    this.dataSharedService.setAgoraStatus(false);
   }
 
   startCall() {
     this.streamClient.init(environment.agoraAppID, () => console.log("AgoraRTC client initialized"), (err) => {
       console.log('err', err);
     });
-    this.streamClient.setClientRole("host", function (e) {
+    this.streamClient.setClientRole(this.role, function (e) {
       if (!e) {
         console.log("setHost success");
       } else {
         console.log("setHost error", e);
       }
     });
-
-    // this.agoraService.client.join(null, '1000', null, (uid) => {
-    //   this.localStream = this.agoraService.createStream(uid, true, null, null, true, false);
-    //   this.localStream.setVideoProfile('720p_3');
-    //   this.subscribeToStreams();
-    // });
 
     this.streamClient.join(null, this.agenda_id, null, (uid) => {
       // Stream object associated with your web cam is initialized
@@ -74,40 +86,25 @@ export class AgoraComponent implements OnInit {
       console.log("accessDenied");
     });
 
-    // this.localStream.init(() => {
-    //   console.log("getUserMedia successfully");
-    //   this.localStream.play('agora_local');
-    //   this.agoraService.client.publish(this.localStream, function (err) {
-    //     console.log("Publish local stream error: " + err);
-    //   });
-    //   this.agoraService.client.on('stream-published', function (evt) {
-    //     console.log("Publish local stream successfully");
-    //   });
-    // }, function (err) {
-    //   console.log("getUserMedia failed", err);
-    // });
+    if (this.role == "host") {
+      this.localStream.init(() => {
 
+        this.localStream.play('local');
 
-    // Associates the stream to the client
-    this.localStream.init(() => {
-
-      //Plays the localVideo
-      this.localStream.play('local');
-
-      //Publishes the stream to the channel
-      this.streamClient.publish(this.localStream, (err) => {
-        console.log("err@@@@@@@@@@", err);
+        this.streamClient.publish(this.localStream, (err) => {
+          console.log("err@@@@@@@@@@", err);
+        });
+      }, (err) => {
+        console.log("err############", err);
       });
-    }, (err) => {
-      console.log("err############", err);
-    });
+    }
 
     //When a stream is added to a channel
     this.streamClient.on('stream-added', function (evt) {
-      console.log("at host side strea-added", evt);
+      console.log("\non stream added\n", evt);
 
       $this.streamClient.subscribe(evt.stream, (err) => {
-        console.log("err!!!!!!!!!!!!!!!", err);
+        console.log("\nstream subscribe err!!!!!!!!!!!!!!!\n", err);
       });
     });
 
@@ -125,7 +122,7 @@ export class AgoraComponent implements OnInit {
 
     //When a person is removed from the stream
     this.streamClient.on("stream-unpublished", () => {
-      alert('unpublish');
+      console.log("@@@@@@@@@@@@@@@unpublish@@@@@@@@@");
     })
     // client.on('stream-removed', removeVideoStream);
     this.streamClient.on('peer-leave', this.removeVideoStream);
@@ -152,9 +149,11 @@ export class AgoraComponent implements OnInit {
   }
 
   initChat(uid) {
+    let $this = this;
     let userId = uid.toString();
+    let userWithRole = userId + "." + this.role;
     this.chatChannel = this.chatClient.createChannel(this.agenda_id);
-    this.chatClient.login({ token: null, uid: userId }).then(() => {
+    this.chatClient.login({ token: null, uid: userWithRole }).then(() => {
       console.log('AgoraRTM chatClient login success');
       this.chatChannel.join().then(() => {
         console.log(`User id joined a chat channel userId: ${userId} `)
@@ -173,10 +172,26 @@ export class AgoraComponent implements OnInit {
 
     this.chatChannel.on('ChannelMessage', ({ text }, senderId) => { // text: text of the received channel message; senderId: user ID of the sender.
       var textnode = document.createElement("p"); // Create a text node
+      let senderArr = senderId.split(/[.\-_]/);
+      // console.log("arr", senderArr);
+      // if (senderArr[1] == "host") {
+      //   console.log("arr[0]", senderArr[0]);
+      //   console.log("event", $this.evt_data);
+      //   let user = this.searchUserDetails('5f0851a66de63300170d9f7d', $this.evt_data.speakerList);
+      //   console.log("@@@@@@@@@@@@@@@@@@@@@user", user);
+      // }
       textnode.innerHTML = `${senderId} :- ${text}`
       document.getElementById("messageSection").appendChild(textnode); // Append <li> to <ul> with id="myList"
       console.log(`Recive message from user ${senderId} message is ${text}`)
     });
+  }
+
+  searchUserDetails(key, userList) {
+    for (let i = 0; i < userList.length; i++) {
+      if (userList[i].id === key) {
+        return userList[i];
+      }
+    }
   }
 
   sendMessage() {
@@ -212,7 +227,7 @@ export class AgoraComponent implements OnInit {
         $this.removeVideoStream(evt);
       }
 
-      $this.router.navigate(['/dashboard', $this.evt_id, 'talks']);
+      $this.router.navigate(['/dashboard', $this.evt_data.event_id, 'talks']);
     }, function (err) {
       console.log("client leave failed ", err);
     });
